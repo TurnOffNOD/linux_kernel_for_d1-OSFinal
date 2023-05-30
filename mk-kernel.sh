802bbbe076e7dc687a442ff88811da45d76a8217
Thomas Copper <universeCopper@gmail.com>
2023-05-31 02:04:39 +0800

    change to use Xuantie provide gcc instead that from tina-sdk

--- mk-kernel.sh
+++ mk-kernel.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-path=../tina-sdk-d1
-make -j8 ARCH=riscv  CROSS_COMPILE=${path}/lichee/brandy-2.0/tools/toolchain/riscv64-linux-x86_64-20200528/bin/riscv64-unknown-linux-gnu-  \
+path=/opt/huge_softs/Xuantie-900-gcc-elf-newlib-x86_64-V2.4.0
+make -j8 ARCH=riscv  CROSS_COMPILE=${path}/bin/riscv64-unknown-linux-gnu-  \
 && ./rvboards_img/mkbootimg  --kernel  arch/riscv/boot//Image --board  d1-nezha --base  0x40200000 --kernel_offset  0x0 -o  ./rvboards_img/rvboards_boot.img
