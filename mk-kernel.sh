8f7d396212aadbb9e98d69e6bd7e6560cccffad5
Thomas Copper <universeCopper@gmail.com>
2023-05-31 02:12:09 +0800

    correct a typo leading to CROSS_COMPILE-gcc not found

--- mk-kernel.sh
+++ mk-kernel.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 path=/opt/huge_softs/Xuantie-900-gcc-elf-newlib-x86_64-V2.4.0
-make -j8 ARCH=riscv  CROSS_COMPILE=${path}/bin/riscv64-unknown-linux-gnu-  \
+make -j8 ARCH=riscv  CROSS_COMPILE=${path}/bin/riscv64-unknown-elf-linux-gnu-  \
 && ./rvboards_img/mkbootimg  --kernel  arch/riscv/boot//Image --board  d1-nezha --base  0x40200000 --kernel_offset  0x0 -o  ./rvboards_img/rvboards_boot.img
